103d6114bb4685651c603c05492475d3af6f086d
Lukas Bentkamp <lukas.bentkamp@gmail.com>
2015-09-27 20:47:32 +0200

    Einheitensystem zum Laufen gebracht, fehlen noch Vorfaktoren

--- dataAnalyzer.py
+++ dataAnalyzer.py
@@ -1,4 +1,3 @@
-from sympy import Symbol
 import units
 from quantities import *
 import gnuplot
@@ -7,30 +6,10 @@ import latexOutput as out
 import fileInput as inp
 
 
-units.makeUnit("m")
-units.makeUnit("s")
-units.makeUnit("A")
-units.makeUnit("mol")
-units.makeUnit("cd")
-units.makeUnit("kg")
-units.makeUnit("K")
-
-units.makeUnit("mA","A/1000")
-units.makeUnit("mm","m/1000")
-units.makeUnit("Hz","1/s")
-#Newton
-units.makeUnit("Pa","kg/m/s**2")
-units.makeUnit("J","m**2*kg/s**2")
-units.makeUnit("W","m**2*kg/s**3")
-units.makeUnit("C","s*A")
-units.makeUnit("V","m**2*kg/s**3/A")
-units.makeUnit("F","s**4*A**2/m**2/kg")
-units.makeUnit("ohm","m**2*kg/s**3/A**2")
-#...
-
 data={}
-data["m"]=Measurement("m","",np.array([123421.124556,4.0,14]),np.array([0.0145,1.3,3]),units.parse_expr("kg"))
-data["b"]=Result("b","",parse_expr("m*2",data))
+data["m"]=Measurement("m","",np.array([123421.124556,4.0,14]),np.array([0.0145,1.3,3]),units.parse_expr("V"))
+data["n"]=Measurement("n","",np.array([123421.124556,4.0,14]),np.array([0.0145,1.3,3]),units.parse_expr("A"))
+data["b"]=Result("b","",parse_expr("2*m/n",data))
 
 out.addQuantity(data["m"])
 out.addQuantity(data["b"])
--- units.py
+++ units.py
@@ -1,4 +1,5 @@
 from sympy import Expr,Symbol
+from sympy.core.mul import Mul
 from sympy.parsing.sympy_parser import parse_expr as sym_parse_expr
 
 # TODO
@@ -14,7 +15,7 @@ from sympy.parsing.sympy_parser import parse_expr as sym_parse_expr
 
 units={}
 
-def makeUnit(name,dependency=False):
+def makeUnit(name,dependency=False,inOutput=True):
 	if not isinstance(name,str):
 		raise TypeError("Einheitenname muss ein String sein.")
 	if not (dependency==False or isinstance(dependency,str)):
@@ -22,9 +23,9 @@ def makeUnit(name,dependency=False):
 	if name in units:
 		raise ValueError("Einheit gibt es schon.")
 	if dependency==False:
-		units[name]=BaseUnit(name)
+		units[name]=BaseUnit(name,inOutput=inOutput)
 	else:
-		units[name]=DerivedUnit(name,parse_expr(dependency))
+		units[name]=DerivedUnit(name,parse_expr(dependency),inOutput=inOutput)
 
 #gibt Einheitenobjekt zurück
 def getUnit(sym):
@@ -46,7 +47,9 @@ def clearUnits(expr):
 
 	#Einheiten expandieren
 	for unit in expr.free_symbols:
-		result=result.subs(unit,unit.expand())
+		result=result.subs(unit,unit.expandUnits())
+
+	result=simplifyUnits(result)
 
 	#Einheiten nach Komplexität ordnen
 	unitList=[]
@@ -56,9 +59,17 @@ def clearUnits(expr):
 
 	#Einheiten nach Komplexitätsstufe einsetzen
 	for unit in sortedList:
-		result=result.subs(unit.expand(),unit)
+		if unit.inOutput():
+			result=result.subs(unit.expandUnits(),unit)			
 
 	return result
+
+def simplifyUnits(expr):
+	newArgs=[]
+	for arg in expr.args:
+		if not (expr.func==Mul and arg.is_number):
+			newArgs.append(arg)
+	return expr.func(*newArgs)
 
 def parse_expr(exprStr):
 	expr=sym_parse_expr(exprStr)
@@ -70,36 +81,63 @@ def parse_expr(exprStr):
 
 class Unit(Symbol):
 
-	def __new__(cls,name):
+	def __new__(cls,name,inOutput):
 		self = Symbol.__new__(cls, name, positive=True)
 		self.abbrev=name
 		self._name=name
+		self._inOutput=inOutput
 		return self
+
+	def inOutput(self):
+		return self._inOutput
 
 	def getComplexity(self):
 		return self._complexity
 	#gibt Einheit in Grundeinheiten an
-	def expand(self):
+	def expandUnits(self):
 		pass
 
 class BaseUnit(Unit):
-	def __new__(cls, name,  **assumptions):
-		self=Unit.__new__(cls,name)
+	def __new__(cls, name,  inOutput):
+		self=Unit.__new__(cls,name,inOutput)
 		self._complexity=1
 		return self
-	def expand(self):
+	def expandUnits(self):
 		return self
 
 class DerivedUnit(Unit):
-	def __new__(cls, name, dependency, **assumptions):
-		self=Unit.__new__(cls,name)
+	def __new__(cls, name, dependency, inOutput):
+		self=Unit.__new__(cls,name,inOutput)
 		self._dependency=dependency
-		self._complexity=len(dependency.free_symbols)
+		expanded=self.expandUnits()
+		self._complexity=0
+		for var in expanded.free_symbols:
+			self._complexity+=abs(expanded.as_coeff_exponent(var)[1])
 
 		return self
 
-	def expand(self):
+	def expandUnits(self):
 		result=self._dependency
 		for unit in self._dependency.free_symbols:
-			result=result.subs(unit,getUnit(unit).expand())
+			result=result.subs(unit,getUnit(unit).expandUnits())
 		return result
+
+makeUnit("m")
+makeUnit("s")
+makeUnit("A")
+makeUnit("mol")
+makeUnit("cd")
+makeUnit("kg")
+makeUnit("K")
+
+makeUnit("mA","A/1000",inOutput=False)
+makeUnit("mm","m/1000",inOutput=False)
+makeUnit("Hz","1/s",inOutput=False)
+#Newton
+makeUnit("Pa","kg/m/s**2")
+makeUnit("J","m**2*kg/s**2")
+makeUnit("W","m**2*kg/s**3")
+makeUnit("C","s*A")
+makeUnit("V","m**2*kg/s**3/A")
+makeUnit("F","s**4*A**2/m**2/kg")
+makeUnit("ohm","m**2*kg/s**3/A**2")
