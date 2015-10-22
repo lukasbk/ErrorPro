44b2540b5e9cb3b378e0c76b61895dee0dffeb26
Lukas Bentkamp <lukas.bentkamp@mailbox.org>
2015-10-22 16:23:36 +0200

    moved mean value to calculations.py

--- commands/MeanValue.py
+++ commands/MeanValue.py
@@ -1,11 +1,6 @@
 import numpy as np
 from quantities import Quantity
-from scipy.stats import t as student_t
-
-# calculate student-t-factor
-def get_t_factor(sample_number, confidence_interval = 0.683):
-	one_sided_ci = (confidence_interval + 1) / 2
-	return student_t.ppf(one_sided_ci, sample_number-1 )
+from calculations import standard_mean_value, standard_weighted_mean_value
 
 class MeanValue():
 
@@ -13,7 +8,7 @@ class MeanValue():
 		self.quantity_to_assign = quantity_to_assign
 		self.longname = ""
 		self.quantities = []
-		self.tryWeighted = True
+		self.try_weighted = True
 
 	def execute(self, data, config, output):
 
@@ -27,7 +22,7 @@ class MeanValue():
 		# put all values and uncertainties into arrays
 		values = np.ndarray((0),dtype=np.float_)
 		uncerts = np.ndarray((0),dtype=np.float_)
-		weighted = self.tryWeighted
+		weighted = self.try_weighted
 		dim = None
 		for q in quantities:
 			if q.value is None:
@@ -50,14 +45,12 @@ class MeanValue():
 
 		# mean value calculation
 		if weighted:
-			mean_value = ( values / uncerts**2 ).sum() / ( 1 / uncerts**2 ).sum()
+			mean_value, stat_uncert = standard_weighted_mean_value(values, uncerts)
 			value_depend = "standard weighted mean value"
-			stat_uncert = np.sqrt(1 / (1 / uncerts**2).sum())
 			uncert_depend = "standard weighted mean value error"
 		else:
-			mean_value = values.sum() / len(values)
+			mean_value, stat_uncert = standard_mean_value(values)
 			value_depend = "standard mean value"
-			stat_uncert = get_t_factor(len(values)) * np.sqrt(1 / (len(values) * (len(values)-1) ) * ((values - mean_value)**2).sum() )
 			uncert_depend = "standard mean value error"
 
 		# save things
--- commands/PythonCode.py
+++
@@ -1,6 +0,0 @@
-class PythonCode():
-	def __init__(self, code):
-		self.code = code
-
-	def execute(self, data, config, output):
-		exec(self.code)
