53c32b4ceae6758fd149b1b27a05683c362f2548
Alexander Bentkamp <bentkamp@gmail.com>
2016-05-02 13:57:36 +0200

    2dim-syntax

--- errorpro/interpreter.py
+++ errorpro/interpreter.py
@@ -18,34 +18,46 @@ def interpret (program, namespace):
 			namespace[command.name] = assign (value, error, command.unit, command.name, command.longname)
 
 		elif command.parseinfo.rule == "multi_assignment":
-			# collect columns:
-			columns = {}
-			for columnIndex in range(len(command.header)):
+			if len(command.header) == 1: # only one header => one 2-dimensional dataset
 				values = []
 				for row in command.rows:
-					value = parse_expr(row[columnIndex], namespace)
-					values.append(value)
-				columns[command.header[columnIndex].name] = {
-				 "header": command.header[columnIndex],
-				 "values": values
-				}
-			# pair value columns with err-columns:
-			for name in columns:
-				if name.endswith("_err"):
-					continue
-				header = columns[name]["header"]
-				values = columns[name]["values"]
-				if name + "_err" in columns:
-					errorColumn = columns[name + "_err"]
-					if errorColumn["header"].error is not None:
-						raise RuntimeError("Variables with _err notation cannot use the <...> notation:  %s"%errorColumn["header"].name)
-					if errorColumn["header"].longname is not None:
-						raise RuntimeError("Variables with _err notation cannot have a long name: %s"%errorColumn["header"].longname)
-					if header.error is not None:
-						raise RuntimeError("Variables with a corresponding _err column cannot have a general error specified: %s"%header.name)
-					namespace[name] = assign (values, errorColumn["values"], header.unit, name, header.longname, None, errorColumn["header"].unit)
-				else:
-					namespace[header.name] = assign (values, header.error, header.unit, header.name, header.longname)
+					row_values = []
+					for entry in row:
+						value = parse_expr(entry, namespace)
+						row_values.append(value)
+					values.append(row_values)
+				header = command.header[0]
+				namespace[header.name] = assign (values, header.error, header.unit, header.name, header.longname)
+
+			else: # more than one header => multiple 1-dimensional datasets
+				# collect columns:
+				columns = {}
+				for columnIndex in range(len(command.header)):
+					values = []
+					for row in command.rows:
+						value = parse_expr(row[columnIndex], namespace)
+						values.append(value)
+					columns[command.header[columnIndex].name] = {
+					 "header": command.header[columnIndex],
+					 "values": values
+					}
+				# pair value columns with err-columns:
+				for name in columns:
+					if name.endswith("_err"):
+						continue
+					header = columns[name]["header"]
+					values = columns[name]["values"]
+					if name + "_err" in columns:
+						errorColumn = columns[name + "_err"]
+						if errorColumn["header"].error is not None:
+							raise RuntimeError("Variables with _err notation cannot use the <...> notation:  %s"%errorColumn["header"].name)
+						if errorColumn["header"].longname is not None:
+							raise RuntimeError("Variables with _err notation cannot have a long name: %s"%errorColumn["header"].longname)
+						if header.error is not None:
+							raise RuntimeError("Variables with a corresponding _err column cannot have a general error specified: %s"%header.name)
+						namespace[name] = assign (values, errorColumn["values"], header.unit, name, header.longname, None, errorColumn["header"].unit)
+					else:
+						namespace[header.name] = assign (values, header.error, header.unit, header.name, header.longname)
 
 		elif command.parseinfo.rule == "python_code":
 			code = '\n'.join(command.code)
