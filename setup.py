4ff46d092fd43d91c6e88bc67058b438eac01d98
Lukas Bentkamp <lukas.bentkamp@gmail.com>
2016-05-16 21:52:19 +0200

    setup file

--- setup.py
+++ setup.py
@@ -10,7 +10,7 @@ def read(fname):
 
 setup(
     name = "errorpro",
-    version = "0.1.3",
+    version = "0.1.4",
     author = "Lukas Bentkamp",
     author_email = "lukas.bentkamp@mailbox.org",
     description = ("calculates physical quantities from data including units and error propagation."),
